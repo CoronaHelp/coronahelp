df528d052ecf1125c9a7963767fa9b570beace3a
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-03-30 23:15:38 -0600

    Sign up and login forms operational

---
+++ src/components/formComponents/LoginForm.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect }from 'react'
+import * as yup from 'yup'
+import axios from 'axios'
+import { Button, FormSection, Form, Label, Input, Error } from './styledFormComponents';
+
+const LoginForm = () => {
+
+    const [submitDisabled, setSubmitDisabled] = useState(true)
+
+    const [post, setPost] = useState([])
+
+    const [formState, setFormState] = useState({
+        username: "",
+        password: "",
+    })
+
+    const [formErrors, setFormErrors] = useState({
+        username: "",
+        password: "",
+    })
+
+    useEffect(() => {
+        formSchema.isValid(formState)
+            .then(valid => {
+                setSubmitDisabled(!valid)
+                
+            })
+    }, [formState, submitDisabled])
+
+    const formSchema = yup.object().shape({
+        username: yup.string()
+            .required("Please input your username."),
+        password: yup.string()
+            .min(6, "This password must be at least 6 characters long")
+            .required("Please input a password of at least 6 characters"),
+    })
+
+    const validateData = event => {
+        yup
+            .reach(formSchema, event.target.name)
+            .validate(event.target.value)
+            .then(valid => {
+                console.log(valid)
+                
+                setFormErrors({
+                    ...formErrors,
+                    [event.target.name]: ""
+                })
+            })
+            .catch(error => {
+                console.log(error)
+                setFormErrors({
+                  ...formErrors,
+                  [event.target.name]: error.errors[0]
+                })
+            })
+    }
+
+    const updateForm = event => {
+        event.persist()
+        validateData(event)
+        setFormState({
+            ...formState, 
+            [event.target.name]: event.target.value
+        })
+        console.log(formErrors)
+      }
+
+    const submitForm = event => {
+        event.preventDefault()
+        axios
+          .post("https://reqres.in/api/users", formState)
+          .then(response => {
+            console.log("Posted successfully: ", post)
+            setPost(response.data)
+            setFormState({
+                username: "",
+                password: "",
+            })
+          })
+          .catch(error => console.log("Post was not successful: ", error.response))
+    }
+
+    return (
+        <Form onSubmit={submitForm}>
+            <FormSection>
+                <Label htmlFor="username"> * Username</Label>
+                <Input id="username" name="username" type="text" placeholder="username" value={formState.name}  onChange={updateForm}/>
+                {formErrors.username ? <Error>{formErrors.username}</Error> : null}
+            </FormSection>
+            <FormSection>
+                <Label htmlFor="password"> * Passwords</Label>
+                <Input id="password" name="password" type="password" placeholder="password" value={formState.password} onChange={updateForm}/>
+                {formErrors.password ? <Error>{formErrors.password}</Error> : null} 
+            </FormSection> 
+            <Button disabled={submitDisabled}>Create Account</Button>
+        </Form>
+    )
+}
+export default LoginForm
---
+++ src/components/formComponents/styledFormComponents.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+
+export const FormSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
+    width: 340px;
+    margin: 4px 0;
+`;
+
+// ${theme.breakpoints.tablet}{
+//     flex-direction: ${props => props.checkbox ? 'row' : 'column'}
+// }
+
+export const Button = styled.button`
+    background-color: ${props => props.disabled ? 'grey' : '#7F7FFF'};
+    color: white;
+    width: 150px;
+    height: 35px;
+    font-size: 1rem;
+    border: 2px solid white;
+    border-radius: 5px;
+    cursor: pointer;
+    margin: 15px 0;
+`
+
+export const Form = styled.form`
+    width: 340px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+`;
+
+// ${theme.breakpoints.tablet}{
+//     width: ${props => props.auth ? '340px' : '900px'};
+//     flex-direction: ${props => props.auth ? 'column' : 'row'};
+//     flex-wrap: wrap;
+// }
+
+export const Input = styled.input`
+    width: 100%;
+    max-width: ${props => props.search ? '180px' : '320px'};
+    min-width: ${props => props.search ? '180px' : '280px'};
+    background-color: ${props => props.submit ? (props.disabled ? 'grey' : 'red') : 'white'};
+    color: ${props => props.submit ? 'white' : 'black'};
+    height: 30px;
+    border: 1px solid #7F7FFF;
+    outline: none;
+    border-radius: 5px;
+    
+    -webkit-appearance: none;
+    padding: 0 10px;
+    font-size: 1.2rem;
+    font-family: 'sans-serif';
+    cursor: ${props => props.submit ? 'pointer' : 'text'};
+`;
+
+export const Label = styled.label`
+    font-size: 1rem;
+    font-family: 'sans-serif';
+    margin: 1px 0 3px 5px;
+`;
+
+export const Error = styled.p`
+    font-size: 1.0rem;
+    font-family: 'sans-serif';
+    color: red;
+`;
