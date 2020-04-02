2d4a3e9bb1655d0b938a30e62394b8660b1d459f
david-pok <david.pok@gmail.com>
2020-04-02 11:26:29 -0700

    setup register form

---
+++ src/utility/AxiosWithAuth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const AxiosWithAuth = () => {
+  const token = localStorage.getItem("token");
+
+  return axios.create({
+    baseURL: "https://supplyhelper-be-staging.herokuapp.com/",
+    headers: {
+      Authorization: token
+    }
+  });
+};
+
+export default AxiosWithAuth;
