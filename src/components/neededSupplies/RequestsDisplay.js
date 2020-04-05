b80e06820e8272b8d4e91c9c0685eff99a9b565d
Carlos Vargas <cornielleandres@gmail.com>
2020-04-04 22:42:46 -0400

    Update how requests are displayed.

--- src/components/neededSupplies/RequestsDisplay.js
+++ src/components/neededSupplies/RequestsDisplay.js
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react";
-import Requests from "./Request"
-import "../requests.css"
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
+import "../requests.css";
+
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const RequestsDisplay = () => {
   const [request, setRequest] = useState([]);
-  const [zip, setzip] = useState([])
-  const [search, setSearch] = useState([])
-
+  const [zip, setzip] = useState([]);
   const [modal, setModal] = useState(false);
-  const handleChange = e => {
-    setzip(e.target.value);
-  };
-  useEffect( ()=>{
-    const fetch = async ()=>{
-    const result = await axios.get(`https://supplyhelper-be-staging.herokuapp.com/api/requests/all/${search}/25`)
-    .then((res)=>{
-      console.log("My Res : ",res)
-      setRequest(res.data)
-    })
-    .catch((err)=>console.log("my error : " + err))
-    }
-    fetch()
-  },[search])
+  const [requestInModal, setRequestInModal] = useState(null);
+  const handleChange = e => setzip(e.target.value);
+  const backendURL = process.env.REACT_APP_BACKEND_URL;
+  const url = `${ backendURL }/api/requests/all/${ zip }/25`;
+  const searchRequests = () => (
+    axios.get(url)
+      .then(res => {
+        console.log("res.data", res.data)
+        setRequest(res.data)
+      })
+      .catch(err => console.log("my error:" + err))
+  );
   const toggle = () => setModal(!modal);
- 
+  const handleClick = requestItem => {
+    setRequestInModal(requestItem);
+    toggle();
+  };
   return (
     <div className = "requestContainer">
       <input
@@ -33,20 +33,32 @@ const RequestsDisplay = () => {
         value={zip}
         onChange={handleChange}
       />
-      <button onClick = {()=>setSearch(zip)} > Submit</button>
-      
-      {request.map(t => {
-        return(
-        <div  className = "mycontainer">
-        <h1 onClick = {()=>toggle()}>{t.title}</h1>
-        <Requests toggler ={toggle} title = {t.title} description = {t.message} modal={modal}/>
+      <button onClick = { searchRequests }>Submit</button>
+      { request.length ?
+        <div>
+          {request.map((r, i) => (
+            <div key = { i } className = "mycontainer">
+              <h1 onClick = { () => handleClick(r) }>{r.title}</h1>
+            </div>
+          ))}
         </div>
-      )})}
+        :
+        <p>No requests found.</p>
+      }
+      { requestInModal &&
+        <Modal isOpen={modal} toggler={toggle} className="danger">
+          <ModalHeader toggle={toggle}>{requestInModal.title}</ModalHeader>
+          <ModalBody>
+          {requestInModal.description || 'No description provided.'}
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      }
     </div>
   );
-  
-
-}
+};
 
 export default RequestsDisplay;
-
